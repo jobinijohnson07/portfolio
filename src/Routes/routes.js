d10bafd446ccf86400cfc682ac49869b0c1606e0
jobinijohnson07 <jobinijohnson981@gmail.com>
2025-05-22 22:10:39 +0530

    first commit

---
+++ src/Routes/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Routes} from 'react-router-dom';
+import { BackgroundLinesDemo } from '../components/LandingPage/LandingPage.js';
+
+const RouteComponent = () => {
+  return (
+    <>
+      <BackgroundLinesDemo />
+    </>
+  );
+}
+
+export default RouteComponent;
