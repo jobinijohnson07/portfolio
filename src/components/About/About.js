052c33df88f4a28f64ddb451a583430239aef7a3
jobinijohnson07 <jobinijohnson981@gmail.com>
2025-08-22 21:53:42 +0530

    about changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,18 +6,18 @@ const About = () => {
     <div className="about-section bg-white dark:bg-black text-black dark:text-white">
       <div className="display-section">
         <div className="image-section">
-          <img src={DevImg} alt="devImg" />
+          <img src={DevImg} alt="Developer" />
         </div>
         <div className="content-section">
-          <div className="aboutheading-content text-neutral-700 dark:text-neutral-400">
+          <h2 className="aboutheading-content text-neutral-700 dark:text-neutral-400">
             About Me
-          </div>
-          <div className="about-content max-w-xl text-sm md:text-lg text-neutral-700 dark:text-neutral-400">
+          </h2>
+          <p className="about-content max-w-xl text-sm md:text-lg text-neutral-700 dark:text-neutral-400">
             I'm a Senior Front-End Developer with a passion for creating seamless digital experiences using ReactJS, 
             AngularJS, JavaScript, and TypeScript. I specialize in turning complex problems into clean, scalable solutions. 
             With strong skills in API development and testing, I thrive in fast-paced, collaborative teams. I also write 
             technical blogs to share insights, best practices, and real-world challenges to help grow the dev community.
-          </div>
+          </p>
           <a
             href="/JobinyJohnsonResume.pdf"
             target="_blank"
